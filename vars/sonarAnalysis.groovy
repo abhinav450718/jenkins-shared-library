8de19cd0128aed85eeeb9dacfb7bf5bc96e64a8c
abhinav450718 <abhinavsikarwar011@gmail.com>
2026-04-18 20:01:53 +0530

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,14 +2,14 @@ def call(Map config) {
 
     node {
 
-        def repoUrl      = config.repoUrl
-        def branch       = config.branch ?: 'main'
-        def credId       = config.gitCredentialsId ?: ''
-        def slackCh      = config.slackChannel ?: '#ci-operation-notifications'
-        def email        = config.email ?: ''
-        def GO_VERSION   = config.goVersion ?: '1.22.5'
-        def sonarKey     = config.sonarProjectKey ?: 'employee-api'
-        def sonarName    = config.sonarProjectName ?: 'Employee API'
+        def repoUrl    = config.repoUrl
+        def branch     = config.branch ?: 'main'
+        def credId     = config.gitCredentialsId ?: ''
+        def slackCh    = config.slackChannel ?: '#ci-operation-notifications'
+        def email      = config.email ?: ''
+        def GO_VERSION = config.goVersion ?: '1.22.5'
+        def sonarKey   = config.sonarProjectKey ?: 'employee-api'
+        def sonarName  = config.sonarProjectName ?: 'Employee API'
 
         def SONAR_VERSION = '5.0.1.3006'
         def REPORT_DIR    = 'reports'
@@ -30,6 +30,7 @@ def call(Map config) {
 
             stage('Setup Go') {
                 sh """
+                    set -e
                     if [ ! -f "${GO_DIR}/bin/go" ]; then
                         mkdir -p ${TOOLS_DIR}
                         curl -sLO https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
@@ -43,6 +44,7 @@ def call(Map config) {
 
             stage('Setup SonarScanner') {
                 sh """
+                    set -e
                     if [ ! -f "${SONAR_DIR}/bin/sonar-scanner" ]; then
                         mkdir -p ${TOOLS_DIR}
                         curl -sLO https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_VERSION}-linux.zip
@@ -64,6 +66,7 @@ def call(Map config) {
 
             stage('Generate Coverage') {
                 sh """
+                    set -e
                     export GOROOT=${GO_DIR}
                     export GOPATH=\$HOME/go
                     export PATH=\$GOROOT/bin:\$GOPATH/bin:\$PATH
@@ -108,44 +111,18 @@ def call(Map config) {
 
             def status = currentBuild.result ?: 'FAILURE'
 
-            def testLog   = "${env.BUILD_URL}artifact/${REPORT_DIR}/test.log"
-            def coverage  = "${env.BUILD_URL}artifact/${REPORT_DIR}/coverage_summary.txt"
-            def sonarLog  = "${env.BUILD_URL}artifact/${REPORT_DIR}/sonar.log"
-
-            def message = """
-*${status}* - Go SonarQube Analysis
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Job: ${env.JOB_NAME}
-Build: #${env.BUILD_NUMBER}
-Branch: ${branch}
-Status: ${status}
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-<${env.BUILD_URL}|View Build> |
-<${testLog}|Test Log> |
-<${coverage}|Coverage Summary> |
-<${sonarLog}|Sonar Log>
-"""
-
-            commonUtils.sendSlack(slackCh, message, status)
-
-            commonUtils.sendEmail(
-                email,
-                "${status}: Sonar Analysis",
-                """
-                <h3>${status} - Go SonarQube Analysis</h3>
-                <p><b>Job:</b> ${env.JOB_NAME}</p>
-                <p><b>Build:</b> #${env.BUILD_NUMBER}</p>
-                <p><b>Branch:</b> ${branch}</p>
-                <p><b>Status:</b> ${status}</p>
-
-                <p>
-                    <a href="${env.BUILD_URL}">View Build</a> |
-                    <a href="${testLog}">Test Log</a> |
-                    <a href="${coverage}">Coverage Summary</a> |
-                    <a href="${sonarLog}">Sonar Log</a>
-                </p>
-                """,
-                "${REPORT_DIR}/**"
+            commonUtils.notify(
+                status: status,
+                toolName: "Go SonarQube Analysis",
+                branch: branch,
+                slackChannel: slackCh,
+                email: email,
+                reports: [
+                    "Test Log"         : "${REPORT_DIR}/test.log",
+                    "Coverage Summary" : "${REPORT_DIR}/coverage_summary.txt",
+                    "Sonar Log"        : "${REPORT_DIR}/sonar.log"
+                ],
+                attachments: "${REPORT_DIR}/**"
             )
 
             cleanWs()
